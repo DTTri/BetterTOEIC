83fec1c7233753232f300281b162d22407c2c353
Tri Duong <duongthuantri159@gmail.com>
2024-10-05 13:20:26 +0700

    Build: Setup shadcn/ui

--- client/src/pages/test/TestsPage.tsx
+++ client/src/pages/test/TestsPage.tsx
@@ -1,11 +1,12 @@
 // This is just a stub code (mock code)
 
 import PageHeader from "../../components/PageHeader";
-
+import { Button } from "@/components/ui/button";
 export default function TestsPage() {
   return (
-    <div className="bg-background">
+    <div className="bg-background flex flex-col gap-8 items-center pt-8">
       <PageHeader />
+      <Button>Testing shadcn</Button>
     </div>
   );
 }
