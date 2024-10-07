e444e45735c8005dd0e2c86c347b6a0864623bf5
Tri Duong <duongthuantri159@gmail.com>
2024-10-07 11:01:08 +0700

    Build&Feat: Config font-family and colors. Implementing components: PageHeader, SearchBar, QuestionsListContainer

--- client/src/pages/test/TestsPage.tsx
+++ client/src/pages/test/TestsPage.tsx
@@ -1,12 +1,17 @@
 // This is just a stub code (mock code)
 
+import SearchBar from "@/components/SearchBar";
 import PageHeader from "../../components/PageHeader";
 import { Button } from "@/components/ui/button";
+import QuestionsListContainer from "@/components/test/QuestionsListContainer";
 export default function TestsPage() {
+  const headingText = "Thư viện đề thi theo đúng chuẩn ETS";
   return (
     <div className="bg-background flex flex-col gap-8 items-center pt-8">
-      <PageHeader />
+      <SearchBar />
+      <PageHeader text={headingText} />
       <Button>Testing shadcn</Button>
+      <QuestionsListContainer />
     </div>
   );
 }
