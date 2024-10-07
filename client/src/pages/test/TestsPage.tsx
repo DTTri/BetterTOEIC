5c99d53b688238b6cb3e3222bed0e82aaae4935f
Nguyen Trinh Vinh Thinh <thinhss123321@gmail.com>
2024-10-08 00:02:07 +0700

    Feat: Implement footer component

--- client/src/pages/test/TestsPage.tsx
+++ client/src/pages/test/TestsPage.tsx
@@ -1,12 +1,12 @@
 // This is just a stub code (mock code)
 
+import Footer from "@/components/Footer";
 import PageHeader from "../../components/PageHeader";
 import { Button } from "@/components/ui/button";
 export default function TestsPage() {
   return (
     <div className="bg-background flex flex-col gap-8 items-center pt-8">
-      <PageHeader />
-      <Button>Testing shadcn</Button>
+      <Footer></Footer>
     </div>
   );
 }
