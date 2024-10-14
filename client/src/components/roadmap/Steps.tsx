1ef92ddfb32d65175fc31ff8770384853cab4cb7
Tri Duong <duongthuantri159@gmail.com>
2024-10-14 20:33:34 +0700

    Feat: Create LevelChart, LevelExplain, and Steps components - RoadmapPage

---
+++ client/src/components/roadmap/Steps.tsx
@@ -0,0 +1,37 @@
+export default function Steps({ currentStep }: { currentStep: number }) {
+  return (
+    <div className="bg-white rounded-2xl p-4">
+      <div className="progress-bar flex items-center gap-0">
+        <div
+          className={`w-14 h-14 rounded-full flex items-center justify-center text-black font-bold text-xl border border-black bg-purple-600`}
+        >
+          1
+        </div>
+        <div
+          className={`w-10 h-2 ${
+            currentStep > 1 ? "bg-purple-600" : "bg-blue-400"
+          }`}
+        ></div>
+        <div
+          className={`w-14 h-14 rounded-full flex items-center justify-center text-black font-bold text-xl border border-black ${
+            currentStep > 1 ? "bg-purple-600" : "bg-blue-400"
+          }`}
+        >
+          2
+        </div>
+        <div
+          className={`w-10 h-2 ${
+            currentStep > 2 ? "bg-purple-600" : "bg-blue-400"
+          }`}
+        ></div>
+        <div
+          className={`w-14 h-14 rounded-full flex items-center justify-center text-black font-bold text-xl border border-black ${
+            currentStep > 2 ? "bg-purple-600" : "bg-blue-400"
+          }`}
+        >
+          3
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/src/pages/test/TestsPage.tsx
+++ client/src/pages/test/TestsPage.tsx
@@ -1,13 +1,21 @@
 // This is just a stub code (mock code)
 
 import { PageHeader, SearchBar } from "@/components";
-import TestCard from "@/components/test/TestCard";
-import TestCardGallery from "@/components/test/TestCardGallery";
+import LevelChart from "@/components/roadmap/LevelChart";
+import LevelExplain from "@/components/roadmap/LevelExplain";
+import Steps from "@/components/roadmap/Steps";
+
 export default function TestsPage() {
   return (
     <div className="bg-background flex flex-col gap-4 items-center py-8">
-      <PageHeader text="Thư viện đề thi theo đúng chuẩn ETS" />
-      <SearchBar />
+      <Steps currentStep={2} />
+      <div className="bg-white rounded-xl p-8 w-5/6 flex flex-col gap-8">
+        <LevelChart />
+        <LevelExplain
+          level="Intermediate"
+          explain="You can understand and use familiar everyday expressions and very basic phrases aimed at the satisfaction of needs of a concrete type."
+        />
+      </div>
     </div>
   );
 }
