69656e814a2e16d4262ecaad19942acd88f48bcf
Nguyen Trinh Vinh Thinh <thinhss123321@gmail.com>
2024-10-23 11:01:44 +0700

    Refactor: add user, vocab, post type

---
+++ client/src/entities/User.ts
@@ -0,0 +1,13 @@
+type User = {
+     _id: string;
+     email: string;
+     username: string;
+     password: string;
+     fullname: string;
+     avatar: string;
+     created_at: string;
+     updated_at: string;
+     isAdmin: boolean;
+}
+
+export default User;
---
+++ client/src/entities/VocabByTopic.ts
@@ -0,0 +1,9 @@
+import Vocab from "./Vocab";
+type VocabByTopic = {
+    _id: string;
+    topic_name: string;
+    vocabs: Vocab[];
+    image_background: string;
+    created_at: string;
+    updated_at: string;
+}
