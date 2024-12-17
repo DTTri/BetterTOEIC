05bd080a9a590ef488864487b73c7b14809d61b3
Nguyen Trinh Vinh Thinh <vinhthinhvippro@gmail.com>
2024-12-17 23:24:08 +0700

    feat: add google analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <App />
-    <SpeedInsights />
   </BrowserRouter>
 );
