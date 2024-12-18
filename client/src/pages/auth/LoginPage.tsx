31e15ddecca6b7b53637ba9a0f36610285f61454
Nguyen Trinh Vinh Thinh <vinhthinhvippro@gmail.com>
2024-12-18 14:29:42 +0700

    feat: update when navigate to login

--- client/src/pages/auth/LoginPage.tsx
+++ client/src/pages/auth/LoginPage.tsx
@@ -1,6 +1,13 @@
 import LoginForm from '@/components/auth/LoginForm'
+import { sUser } from '@/store'
+import { Navigate } from 'react-router-dom';
 
 export default function LoginPage() {
+  const user = sUser.use(cur => cur.info);
+
+  if(user && user._id) {
+    return <Navigate to={'/'} />;
+  }
   return (
     <div className='bg-[#F5F6FA]'>
       <div className="content p-10">
