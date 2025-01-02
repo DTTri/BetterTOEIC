3e3d4d85b121b1a2035779e52c84c627a479e871
Nguyen Trinh Vinh Thinh <vinhthinhvippro@gmail.com>
2025-01-02 11:33:24 +0700

    refactor: update vite building

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,20 +10,9 @@ export default defineConfig({
     },
   },
   build: {
-    rollupOptions: {
-      external: [
-        'react-dropzone-uploader' // Thêm thư viện gây lỗi vào đây
-      ],
-    },
     commonjsOptions: {
-      ignoreTryCatch: false, // Bỏ qua một số kiểm tra
+      ignoreTryCatch: false,
     },
-    terserOptions: {
-      compress: {
-        drop_console: true, // Xóa tất cả console.log
-        drop_debugger: true, // Xóa debugger
-      },
-    }
   },
   esbuild: {
     exclude: /node_modules/,
